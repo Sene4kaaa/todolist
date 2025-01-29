961cc05cd7c436b2b14cf7a75bebd5912c0de80c
Sene4kaaa <senkevich92@mail.ru>
2025-01-29 15:11:11 +0300

    Refactor function deleteTodolist

--- src/App.tsx
+++ src/App.tsx
@@ -57,6 +57,8 @@ export const App = () => {
 
     const deleteTodolist = (todolistId: string) => {
         setTodolists(todolists.filter(todolist => todolist.id !== todolistId))
+        delete tasks[todolistId]
+        setTasks({...tasks})
     }
 
     return (
