abc61364be91e0a491dc0b1f630739a455964df6
Sene4kaaa <senkevich92@mail.ru>
2025-02-24 16:03:09 +0300

    Add case 'create_todolist' in todolistsReducer

--- src/model/todolists-reducer.ts
+++ src/model/todolists-reducer.ts
@@ -1,4 +1,5 @@
 import {Todolist} from "../App.tsx";
+import {v1} from "uuid";
 
 const initialState: Todolist[] = []
 
@@ -6,6 +7,10 @@ export const todolistsReducer = (state: Todolist[] = initialState, action: Actio
     switch (action.type) {
         case 'delete_todolist': {
             return state.filter(todolist => todolist.id !== action.payload.id)
+        }
+        case 'create_todolist': {
+            const newTodolist: Todolist = {id: action.payload.id, title: action.payload.title, filter: 'all'}
+            return [...state, newTodolist]
         }
         default:
             return state
@@ -15,7 +20,16 @@ export const todolistsReducer = (state: Todolist[] = initialState, action: Actio
 export const deleteTodolistAC = (id: string) => {
     return {type: 'delete_todolist', payload: {id}} as const
 }
+export const createTodolistAC = (title: string) => {
+    return {
+        type: 'create_todolist', payload: {
+            id: v1(),
+            title: title
+        }
+    } as const
+}
 
 type DeleteTodolistAction = ReturnType<typeof deleteTodolistAC>
+type CreateTodolistAction = ReturnType<typeof createTodolistAC>
 
-type Actions = DeleteTodolistAction
+type Actions = DeleteTodolistAction | CreateTodolistAction
