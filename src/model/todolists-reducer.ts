b4c8783f90fc65f5432c0843dcbe1d53c358c80d
Sene4kaaa <senkevich92@mail.ru>
2025-02-24 15:27:39 +0300

    Add test in todolists-reducer.test.ts and ad case 'delete_todolist in todolists-reducer.ts'

---
+++ src/model/todolists-reducer.ts
@@ -0,0 +1,18 @@
+import {Todolist} from "../App.tsx";
+
+const initialState: Todolist[] = []
+
+export const todolistsReducer = (state: Todolist[] = initialState, action: Actions): Todolist[] => {
+    switch (action.type) {
+        case 'delete_todolist': {
+            return state.filter(todolist => todolist.id !== action.payload.id)
+        }
+        default:
+            return state
+    }
+}
+
+type Actions = {
+    type: string
+    payload: any
+}
